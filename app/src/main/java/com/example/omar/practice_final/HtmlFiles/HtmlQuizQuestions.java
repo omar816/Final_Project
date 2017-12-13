bcc7504b7f82d8a75b52905802b5c1abeffeee52
Andrew <a.watton08@gmail.com>
2017-12-13 17:30:49 -0500

    Quizzes finished and filled
    Use HTML for the example. The other quizzes have 2 questions each

--- app/src/main/java/com/example/omar/practice_final/HtmlFiles/HtmlQuizQuestions.java
+++ app/src/main/java/com/example/omar/practice_final/HtmlFiles/HtmlQuizQuestions.java
@@ -5,33 +5,50 @@ package com.example.omar.practice_final.HtmlFiles;
  */
 public class HtmlQuizQuestions {
     public String mQuestions[][] = {
-            {
-                "Something has gone horribly wrong. There is no topic for this quiz."
-            },
+        {   // No topic
+            "Something has gone horribly wrong. There is no topic for this quiz."
+        },
 
-            {
-                "What is HTML an acronym for?",
-                "How do you write 'x' as a comment",
-                "Who is making the Web standards?",
-                "Choose the correct HTML element for the largest heading:",
-                "What is the correct HTML element for inserting a line break?",
-                "What is the correct HTML for adding a background color?",
-                "Choose the correct HTML element to define important text",
-                "Choose the correct HTML element to define emphasized text",
-                "Which character is used to indicate an end tag?",
-                "How can you open a link in a new tab/browser window?"
-            },
-            {
-                "What does CSS Stand for?",
-                "How do you colour an element blue in CSS?"
-            }
+        {   // HTML
+            "What is HTML an acronym for?",
+            "How do you write 'x' as a comment",
+            "Who is making the Web standards?",
+            "Choose the correct HTML element for the largest heading:",
+            "What is the correct HTML element for inserting a line break?",
+            "What is the correct HTML for adding a background color?",
+            "Choose the correct HTML element to define important text",
+            "Choose the correct HTML element to define emphasized text",
+            "Which character is used to indicate an end tag?",
+            "How can you open a link in a new tab/browser window?"
+        },
+        {   //CSS
+            "What does CSS Stand for?",
+            "How do you colour an element blue in CSS?"
+        },
+        {   //JavaScript
+            "What's the most common use for JavaScript?",
+            "How would you describe JS as a language?"
+        },
+        {   //Java
+            "How do you write a Java comment?",
+            "Print the string \"Hello World!\""
+        },
+        {   //C++
+            "How do you print to the screen in C++?",
+            "What's new in C++ from original C?"
+        },
+        {   //Python
+            "Which of the following is not a part of the Python philosophy?",
+            "What is Python named after?"
+        }
+
     };
 
     private String mChoices[][][] = {
-        {
+        {   // No topic
             {"There is no quiz", "There is no quiz", "There is no quiz", "There is no quiz"}
         },
-        {
+        {   //HTML
             {"Hypertext Markup Language", "Hypertext MicroLanugage", "Hyper-Link Markup Tensil", "Hotmail"},
             {"<!x>", "###x", "//x", "<!--x-->"},
             {"The World Wide Web Consortium", "Microsoft", "Google", "Mozilla"},
@@ -43,24 +60,63 @@ public class HtmlQuizQuestions {
             {"<", "*", "/", "^"},
             {"<a href=\"url\" target=\"new\">", "<a href=\"url\" new>", "<a href=\"url\" target=\"_blank\">", "No possible way!"},
         },
-        {
+        {   //CSS
             {"Collaborative Stylization Strategy", "Colour Setting Sheet", "Cascading Style Sheet", "Cassidy Stevenson & Smith"},
             {"{color: blue;}","setColor(blue);","<blue>","{color=blue};"}
+        },
+        {   //JavaScript
+            {"Replacing Java programs","Android Applications","DHTML","Robotics"},
+            {"Compiled, low-level, weakly typed", "Interpreted, high-level, dynamic, weakly typed", "Interpreted, high-level, dynamic, strongly typed", "It's just a fancy word for Java"}
+        },
+        {   //Java
+            {"#like this", "--like this", "//like this", "::like this"},
+            {"put message \"Hello World!\"", "System.out.println(\"Hello World!\");", "print.out.(\"Hello World!\")", "writeToScreen(Hello World!)"}
+        },
+        {   //C++
+            {"cout.std.println();","std::cout>>","print();","std::cout<<"},
+            {"The ++", "Object oriented programming", "Operators", "DHTML Support"}
+        },
+        {   //Python
+            {"Beautiful is better than ugly", "Implicit is better than explicit", "Simple is better than complex", "Complex is better than complicated"},
+            {"The creator", "The Myanmar short-tailed python", "The British comedy group, Monty Python", "The Angolan python" }
         }
     };
 
     private String correctAnswers[][] = {
-            {
+            {   //No topic
                 "There is no answer."
             },
-            {
-                "Hypertext Markup Language", "<!--x-->", "The World Wide Web Consortium",
-                "<h1>", "<br>", "<body style=\"background-color:yellow;\">", "<strong>", "<em>", "/",
+            {   //HTML
+                "Hypertext Markup Language",
+                "<!--x-->",
+                "The World Wide Web Consortium",
+                "<h1>",
+                "<br>",
+                "<body style=\"background-color:yellow;\">",
+                "<strong>",
+                "<em>",
+                "/",
                 "<a href=\"url\" target=\"new\">"
             },
-            {
+            {   //CSS
                 "Cascading Style Sheet",
                 "{color: blue;}"
+            },
+            {   //JavaScript
+                "DHTML",
+                "Interpreted, high-level, dynamic, weakly typed"
+            },
+            {   //Java
+                "//like this",
+                "System.out.println(\"Hello World!\");"
+            },
+            {   //C++
+                "std::cout<<",
+                "Object oriented programming"
+            },
+            {   //Python
+                    "Implicit is better than explicit?",
+                    "The British comedy group, Monty Python"
             }
     };
 
